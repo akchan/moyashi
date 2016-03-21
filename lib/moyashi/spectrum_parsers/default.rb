4862964000975efdfbf65ff8c05373e7dd1c7a6a
Satoshi Funayama (akchan) <akchan.acts@gmail.com>
2016-03-21 17:48:31 +0900

    Bugfix

--- lib/moyashi/spectrum_parsers/default.rb
+++ lib/moyashi/spectrum_parsers/default.rb
@@ -1,17 +1,23 @@
 class DefaultParser < Moyashi::SpectrumParser::Base
   define_name "default"
 
+  define_description "This is a sample parser. You can find a sample input file in path-to-moyashi/samples foulder."
 
-  define_description "This is a sample parser."
-
+  add_required_label :sample_file_name, white_list: "", uniqueness: true
 
   define_params do |p|
     p.file :spectrum, presence: true
   end
 
-
   define_parser do |record, params|
-    record.spectrum = params[:spectrum].read.chomp.split(",").map(&:to_i)
+    raw_spectrum = params[:spectrum].read
+
+    mzs         = raw_spectrum.chomp.split("\n")[0].split(",").map{|str| str.to_f }
+    intensities = raw_spectrum.chomp.split("\n")[1].split(",").map{|str| str.to_i }
+
+    record.spectrum         = mzs.zip(intensities)
+    record.sample_file_name = params[:spectrum].original_filename
+
     record
   end
 end
